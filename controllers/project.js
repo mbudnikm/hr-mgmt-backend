c077f8de5d2853088e520ed01165635873911829
Marta Budnik <mbudnikm97@gmail.com>
2019-11-09 23:28:41 +0100

    add post project

--- controllers/project.js
+++ controllers/project.js
@@ -42,3 +42,41 @@ exports.putProject = (req, res, next) => {
             next(err)
         })
 }
+
+exports.postProject = (req, res, next) => {
+    const project_name = req.body.project_name
+    const project_description = req.body.project_description
+    const project_start_date = req.body.project_start_date
+    const project_end_date = req.body.project_end_date
+    const project_comments = req.body.project_comments
+    const employees = req.body.employees
+
+    const project = new Project({
+        project_name: project_name,
+        project_description: project_description,
+        project_start_date: project_start_date,
+        project_end_date: project_end_date,
+        project_comments: project_comments,
+        employees: employees
+    })
+
+    project
+        .save()
+        .then(result => {
+            console.log(result)
+            res.status(201).json({
+                message: "Projekt został dodany",
+                project: result
+            })
+        })
+        .catch(err => {
+            if(!err.statusCode) {
+                err.statusCode = 500
+            }
+            next(err)
+        })
+}
+
+exports.deleteProject = (req, res, next) => {
+
+}
