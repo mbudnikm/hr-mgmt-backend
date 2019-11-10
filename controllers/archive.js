6db35e9ddfccdb834de40563762fc3f9f8b71743
Marta Budnik <mbudnikm97@gmail.com>
2019-11-10 10:46:35 +0100

    post archive

--- controllers/archive.js
+++ controllers/archive.js
@@ -1,4 +1,5 @@
 const Archive = require('../models/archive')
+const Employee = require('../models/employee')
 
 exports.getArchive = (req, res, next) => {
     Archive
@@ -13,3 +14,35 @@ exports.getArchive = (req, res, next) => {
             next(err)
         })
 };
+
+exports.postArchive = (req, res, next) => {
+    const employee_id = req.body.employee_id
+    const firstname = req.body.firstname
+    const lastname = req.body.lastname
+
+    const archiveEmployee = new Archive({
+        employee_id: employee_id,
+        firstname: firstname,
+        lastname: lastname
+    })
+
+    Employee.findOneAndUpdate(
+        {employee_id: employeeId}, 
+        {archive_id: employeeId},
+        {new: true})
+    && archiveEmployee
+        .save()
+        .then(result => {
+            console.log(result)
+            res.status(201).json({
+                message: "Pracownik został przeniesiony do archiwum danych",
+                archive: result
+            })
+        })
+        .catch(err => {
+            if(!err.statusCode) {
+                err.statusCode = 500
+            }
+            next(err)
+        })
+}
