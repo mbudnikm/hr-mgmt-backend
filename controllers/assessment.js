2e5dcb7efeca351977c5a7be8fe009c269b370ca
Marta Budnik <mbudnikm97@gmail.com>
2019-11-11 11:05:28 +0100

    post assessment

--- controllers/assessment.js
+++ controllers/assessment.js
@@ -14,3 +14,37 @@ exports.getEmployeeAssessments = (req, res, next) => {
             next(err)
         })
 };
+
+
+
+exports.postAssessment = (req, res, next) => {
+    const employee_id = req.body.employee_id
+    const value = req.body.value
+    const year = req.body.year
+    const period = req.body.year
+    const assessment_comment = req.body.assessment_comment
+
+    const assessment = new Assessment({
+        employee_id: employee_id,
+        value: value,
+        year: year,
+        period: period,
+        assessment_comment: assessment_comment
+    })
+
+    assessment
+        .save()
+        .then(result => {
+            console.log(result)
+            res.status(201).json({
+                message: "Ocena została dodana",
+                assessment: result
+            })
+        })
+        .catch(err => {
+            if(!err.statusCode) {
+                err.statusCode = 500
+            }
+            next(err)
+        })
+}
